aac5a8f303a89e5fca3b940e1650feabb99abbee
michelle <gmichele498@gmail.com>
2024-06-24 16:20:11 -0300

    User insertion with POST

--- src/main/java/com/michelleGomes/workshopmongo/services/UserService.java
+++ src/main/java/com/michelleGomes/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.michelleGomes.workshopmongo.domain.User;
+import com.michelleGomes.workshopmongo.dto.UserDTO;
 import com.michelleGomes.workshopmongo.repository.UserRepository;
 import com.michelleGomes.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -24,5 +25,13 @@ public class UserService {
 		Optional<User> user = repository.findById(id);
 		
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repository.insert(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
