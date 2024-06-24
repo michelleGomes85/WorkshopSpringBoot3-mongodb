ecc22c25f11510241a473d059c2518abd0e3abaa
michelle <gmichele498@gmail.com>
2024-06-24 16:05:03 -0300

    Getting a user by id

--- src/main/java/com/michelleGomes/workshopmongo/services/UserService.java
+++ src/main/java/com/michelleGomes/workshopmongo/services/UserService.java
@@ -1,20 +1,28 @@
 package com.michelleGomes.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.michelleGomes.workshopmongo.domain.User;
 import com.michelleGomes.workshopmongo.repository.UserRepository;
+import com.michelleGomes.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-	
+
 	public List<User> findAll() {
 		return repository.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> user = repository.findById(id);
+		
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
